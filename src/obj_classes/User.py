1218ea64f72e0cc6d03c98b3c8945b1b1477860c
luccamoura101 <luccamoura101@gmail.com>
2025-05-04 16:36:14 +0200

    -Changed "new" to "self" in User.py -Removed logic from User.py -Added tests to see if there is any missing info

--- src/obj_classes/User.py
+++ src/obj_classes/User.py
@@ -1,19 +1,15 @@
 # Moaz Ashry 3/5/2025
 
 class User:
-    def __init__(new):
-        new.users = {}
+    def __init__(self, name, user_id):
 
-    def add_users(new, user_id,name):
-        if user_id not in new.users:
-            new.users[user_id] = {"name": name, "isbns": []}
-        else:
-            print ("user ID already exists.")
-    def add_isbn_to_user(new, user_id, isbn):
-        if user_id in new.users:
-            new.users[user_id]["isbns"].append(isbn)
-        else:
-            print("user doesn't exist.")
+        if not isinstance(name, str) or not name:
+            raise ValueError("Missing Name")
+        if not isinstance(user_id, str) or not user_id:
+            raise ValueError("Missing User ID")
 
-    def get_user_info(new, user_id):
-        return new.users.get(user_id, "user not found.")
+        self.user_id = user_id
+        self.name = name
+
+    def get_user_info(self, user_id):
+        return self.users.get(user_id, "user not found.")
