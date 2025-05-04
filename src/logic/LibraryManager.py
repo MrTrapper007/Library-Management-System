7a760bb4bb573eb30ed83018b5931e13b4a8f6c0
luccamoura101 <luccamoura101@gmail.com>
2025-05-04 14:49:23 +0200

    almost done with the libraby management class, just missing borrow and returning methods

--- src/logic/LibraryManager.py
+++ src/logic/LibraryManager.py
@@ -10,6 +10,8 @@ class LibraryManager:
         self.user_books = {}
         self.book_users = {}
 
+# --- Book management system ---
+
     def add_book(self, book):
         # adding books to ur library
         if book.isbn in self.books:
@@ -21,19 +23,112 @@ class LibraryManager:
             print(f"Added book: '{book.title}' (ISBN: {book.isbn})")
 
     def remove_book(self, isbn):
-        # removing books from the library
+        # removing copies of a book from the library
+
         if isbn not in self.books:
             print(f"Error: Book with ISBN {isbn} not found")
             return False
-        if isbn in self.book_users:
-            print(f"Error: Book with ISBN {isbn} is borrowed, can't remove")
+
+        book = self.books[isbn]
+
+        if book.available_copies == 0:
+            # check for any available copies to remove
+            print(f"Error: No available copies of '{book.title}' (ISBN: {book.isbn})")
             return False
-        else:
+
+        book.total_copies -= 1
+        book.available_copies -= 1
+
+        if book.total_copies == 0:
+            book.title = book.title
             del self.books[isbn]
-            print(f"Removed book: '{self.books[isbn].title}' (ISBN: {isbn})")
+
+            if isbn in self.book_users:
+                del self.book_users[isbn]
+
+            print(f"Removed the last copy of '{book.title}' (ISBN: {book.isbn}")
             return True
 
-# Created by Lucca 03/05/25
+        else:
+            print(f"Removed one copy of '{book.title}' (ISBN: {book.isbn})")
+            return True
+
+# Created by Lucca 04/05/25
+
+    def find_book_by_isbn(self, isbn):
+        # finds book by isbn
+        return self.books.get(isbn, None)
+
+    def list_all_books(self):
+        # prints all the books in the library
+        if not self.books:
+            print("No books found")
+            return
+        print ("\n--- Library Book Collection ---")
+
+        sorted_books = sorted(self.books.values(), key=lambda books: book.title)
+
+        for book in sorted_books:
+            # print book details
+            print (book)
+
+            borrowed_by = self.book_users.get(book.isbn, [])
+
+            if borrowed_by:
+                print(f" Borrowed by User Ids: { ', '.join(borrowed_by)}")
+
+            print("-" * 30) #separator line
+
+        print ("----------------------------------") # final separator line
+
+# --- User management system ---
+
+    def add_user(self, user):
+        #adds new user to the system
+        if user.id in self.users:
+            print(f"Error: User with ID '{user.id}' already exists")
+            return False
+
+        self.users[user.id] = user
+        print(f"Added user: '{user.username}' (ID: {user.id})")
+        return True
+
+    def find_user_by_id(self, user_id):
+        #finds user by their ID
+        return self.users.get(user_id, None)
+
+    def list_all_users(self):
+
+        if not self.users:
+            print("No users registered in the system")
+            return
+
+        print ("\n--- Library Users ---")
+        sorted_users = sorted(self.users.values(), key=lambda users: user.name)
+
+        for user in sorted_users:
+            print(f"ID: {user.id}, Name: {user.name}")
+
+            borrowed_isbns = self.book_users.get(user.id, [])
+
+            if borrowed_isbns:
+                #looks up titles for borrowed books
+                borrowed_titles_info = []
+                for isbn in borrowed_isbns:
+                    book = self.find_book_by_isbn(isbn)
+                    if book:
+                        borrowed_titles_info.append(f"'{book.title}' (ISBN: {isbn})")
+                    else:
+                        borrowed_titles_info.append(f"Unknown Book (ISBN: {isbn})")
+
+                print (f"  Borrowed: {', '.join(borrowed_titles_info)}")
+
+            print ("-" * 30)
+
+        print ("-----------------------------------")
+
+
+# --- Borrowing and Returning Methods ---
 
 
 
